3420927b337cb8f78e03d8429fe2a62b0bdc7c62
David Vo <auscompgeek@users.noreply.github.com>
2019-01-29 23:11:16 +1100

    Fix setSecondaryCurrentLimit in simulation

--- tests/test_canspark.py
+++ tests/test_canspark.py
@@ -1,2 +1,17 @@
 def test_init(rev):
     rev.CANSparkMax(0, rev.MotorType.kBrushless)
+
+
+def test_current_limit(rev, hal_data):
+    sm = rev.CANSparkMax(1, rev.MotorType.kBrushless)
+
+    sm.setSecondaryCurrentLimit(50)
+
+    assert hal_data["CANSparkMax"][1]["currentChop"] == 50.0
+    assert isinstance(hal_data["CANSparkMax"][1]["currentChop"], float)
+    assert hal_data["CANSparkMax"][1]["currentChopCycles"] == 0
+
+    sm.setSecondaryCurrentLimit(52.5, 5)
+
+    assert hal_data["CANSparkMax"][1]["currentChop"] == 52.5
+    assert hal_data["CANSparkMax"][1]["currentChopCycles"] == 5
