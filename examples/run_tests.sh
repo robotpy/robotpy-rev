70c40a6da8254b9725a6a091b7181f4a817388c4
sciencewhiz <sciencewhiz@users.noreply.github.com>
2025-01-24 18:13:59 -0800

    Add testing for examples
    Fix getting-started, can-tank-drive, and can-arcade-drive for 2025
    revlib
    Add header check and fix licenses

---
+++ examples/run_tests.sh
@@ -0,0 +1,58 @@
+#!/bin/bash -e
+
+cd "$(dirname $0)"
+
+# Keep this list alphabetically sorted
+BASE_TESTS="
+  can-arcade-drive
+  can-tank-drive
+  getting-started
+"
+
+IGNORED_TESTS="
+  color_match
+  get-set-params
+  limit-switch
+  maxswerve
+  position-pid-control
+  read_rgb_values
+  read-encoder-values
+  smart-motion
+  velocity-pid-control
+"
+
+ALL_TESTS="${BASE_TESTS}"
+EVERY_TESTS="${ALL_TESTS} ${IGNORED_TESTS}"
+TESTS="${ALL_TESTS}"
+
+TMPD=$(mktemp -d)
+trap 'rm -rf "$TMPD"' EXIT
+
+# Ensure that when new samples are added, they are added to the list of things
+# to test. Otherwise, exit.
+for i in ${EVERY_TESTS}; do
+  echo ./$i/robot.py
+done | sort > $TMPD/a
+
+find . -name robot.py | sort > $TMPD/b
+
+if ! diff -u $TMPD/a $TMPD/b; then
+
+  if [ -z "$FORCE_ANYWAYS" ]; then
+    echo "ERROR: Not every robot.py file is in the list of tests!"
+    exit 1
+  fi
+fi
+
+for t in ${TESTS}; do
+  pushd $t > /dev/null
+  pwd
+  if ! python -m robotpy test --builtin "${@:2}"; then
+    EC=$?
+    echo "Test in $(pwd) failed"
+    exit 1
+  fi
+  popd > /dev/null
+done
+
+echo "All tests successful!"
