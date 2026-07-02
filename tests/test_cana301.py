07f59a4385099a4169cfb1a97bafc30956f5d234
Dave Oleksy <dave.oleksy@revrobotics.com>
2026-07-02 14:27:47 -0500

    Update to REVLib 2027.0.0.-alpha-4

--- tests/test_cana301.py
+++ tests/test_cana301.py
@@ -6,6 +6,6 @@ def test_set_velocity():
     a301.setVelocity(12.34)
 
 
-def test_set_position():
+def test_set_relative_position():
     a301 = rev.A301(0, 0)
-    a301.setPosition(12.34)
+    a301.setRelativePositionWithSpeed(12.34, 350.0)
